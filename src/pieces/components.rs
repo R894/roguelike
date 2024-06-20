32645b45f475167ba6aed308ad833a83885e12ca
R894 <kamil.khalaileh@gmail.com>
2024-06-21 00:52:47 +0300

    add pieces

---
+++ src/pieces/components.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+use crate::actions::Action;
+
+#[derive(Component)]
+pub struct Piece {
+    pub kind: String,
+}
+
+#[derive(Component, Default)]
+pub struct Actor(pub Option<Box<dyn Action>>);
+
+#[derive(Component)]
+pub struct Walk;
