9fd27ace3298db8203d22f2d2ed0d9da1632a239
R894 <kamil.khalaileh@gmail.com>
2024-06-22 00:11:40 +0300

    add damageaction

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -11,13 +11,22 @@ pub struct ActionsPlugin;
 impl Plugin for ActionsPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<ActorQueue>()
+            .init_resource::<PendingActions>()
             .add_event::<TickEvent>()
             .add_event::<NextActorEvent>()
             .add_event::<ActionsCompleteEvent>()
             .add_event::<InvalidPlayerActionEvent>()
+            .configure_sets(
+                Update,
+                ActionSet::Planning
+                    .run_if(on_event::<NextActorEvent>())
+                    .before(ActionSet::Late),
+            )
             .add_systems(
                 Update,
-                systems::process_action_queue.run_if(on_event::<TickEvent>()),
+                systems::process_action_queue
+                    .run_if(on_event::<TickEvent>())
+                    .in_set(ActionSet::Late),
             )
             .add_systems(
                 OnExit(GameState::PlayerInput),
@@ -25,14 +34,17 @@ impl Plugin for ActionsPlugin {
             )
             .add_systems(
                 Update,
-                systems::plan_walk.run_if(on_event::<NextActorEvent>()),
+                (systems::plan_walk, systems::plan_melee).in_set(ActionSet::Planning),
             );
     }
 }
 
 pub trait Action: Send + Sync {
-    fn execute(&self, world: &mut World) -> bool;
+    fn execute(&self, world: &mut World) -> Result<Vec<Box<dyn Action>>, ()>;
 }
+
+#[derive(Default, Resource)]
+pub struct PendingActions(pub Vec<Box<dyn Action>>);
 
 #[derive(Default, Resource)]
 pub struct ActorQueue(pub VecDeque<Entity>);
@@ -45,3 +57,9 @@ pub struct NextActorEvent;
 pub struct ActionsCompleteEvent;
 #[derive(Event)]
 pub struct InvalidPlayerActionEvent;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+enum ActionSet {
+    Planning,
+    Late,
+}
