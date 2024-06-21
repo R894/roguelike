e6bd19576ad75bab75ce855ff51f504559566dfc
R894 <kamil.khalaileh@gmail.com>
2024-06-22 00:11:12 +0300

    add player follow logic

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,36 @@
 use bevy::prelude::*;
 
+use crate::{
+    board::components::Position,
+    graphics::{get_world_position, PIECE_SPEED, TILE_SIZE},
+    player::Player,
+    states::MainState,
+};
+
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_camera);
+        app.add_systems(Startup, setup_camera)
+            .add_systems(FixedUpdate, follow_player.run_if(in_state(MainState::Game)));
     }
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn follow_player(
+    mut player_query: Query<&Position, With<Player>>,
+    mut camera_query: Query<&mut Transform, With<Camera>>,
+    time: Res<Time>,
+) {
+    let player_position = player_query.single_mut();
+    let mut camera_transform = camera_query.single_mut();
+
+    let position = get_world_position(player_position, 900.);
+
+    camera_transform.translation = camera_transform
+        .translation
+        .lerp(position, PIECE_SPEED * time.delta_seconds());
 }
