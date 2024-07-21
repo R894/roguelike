9c69dbcb8c85077e6a394fa968ff78e73ac3b837
R894 <kamil.khalaileh@gmail.com>
2024-07-22 01:54:56 +0300

    move cast_line to mod.rs

--- src/vectors/mod.rs
+++ src/vectors/mod.rs
@@ -118,3 +118,66 @@ pub const ORTHO_DIRECTIONS: [Vector2Int; 4] = [
     Vector2Int::LEFT,
     Vector2Int::RIGHT,
 ];
+
+/// casts a line between two points then returns a vector of all the points between the source and the destination or until a blocker is found in the path
+///
+/// uses bresenham's line algorithm
+pub fn cast_line(
+    start: Vector2Int,
+    end: Vector2Int,
+    blocker_positions: &[Vector2Int],
+) -> Vec<Vector2Int> {
+    let mut path = Vec::new();
+    let mut x0 = start.x;
+    let mut y0 = start.y;
+    let x1 = end.x;
+    let y1 = end.y;
+
+    let dx = (x1 - x0).abs();
+    let dy = (y1 - y0).abs();
+    let sx = if x0 < x1 { 1 } else { -1 };
+    let sy = if y0 < y1 { 1 } else { -1 };
+    let mut err = dx - dy;
+
+    loop {
+        // add the current point
+        path.push(Vector2Int::new(x0, y0));
+
+        // return if we've reached a blocker
+        if blocker_positions.contains(&Vector2Int::new(x0, y0)) {
+            return path;
+        }
+
+        // break if we've reached the end
+        if x0 == x1 && y0 == y1 {
+            break;
+        }
+
+        let e2 = 2 * err;
+        if e2 > -dy {
+            err -= dy;
+            x0 += sx;
+        }
+        if e2 < dx {
+            err += dx;
+            y0 += sy;
+        }
+    }
+
+    path
+}
+
+/// line of sight takes a start point and a list of perimeter points and returns a vector of the points in the path of the line of sight
+pub fn line_of_sight(
+    start: Vector2Int,
+    area: Vec<Vector2Int>,
+    blocker_positions: &[Vector2Int],
+) -> Vec<Vector2Int> {
+    let mut path = Vec::new();
+
+    for point in area {
+        path.extend(cast_line(start, point, blocker_positions));
+    }
+
+    path
+}
