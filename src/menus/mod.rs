b166763d1e0cfd48b9b3ccda80d15cc237cb6668
R894 <kamil.khalaileh@gmail.com>
2024-07-26 08:59:48 +0300

    modify main menu to use belly

--- src/menus/mod.rs
+++ src/menus/mod.rs
@@ -3,19 +3,24 @@ pub mod systems;
 
 use bevy::prelude::*;
 use inventory::InventoryPlugin;
+use systems::start_game_event_system;
 
 use crate::states::MainState;
 
 pub struct MenuPlugin;
 
-#[derive(Component)]
-pub struct Menu;
+#[derive(Event)]
+pub struct StartGameEvent;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(InventoryPlugin)
+            .add_event::<StartGameEvent>()
             .add_systems(OnEnter(MainState::Menu), systems::main_menu)
-            .add_systems(Update, systems::menu_button_system)
+            .add_systems(
+                Update,
+                start_game_event_system.run_if(on_event::<StartGameEvent>()),
+            )
             .add_systems(OnExit(MainState::Menu), systems::despawn_menu)
             .add_systems(OnEnter(MainState::GameOver), systems::game_over_menu)
             .add_systems(OnExit(MainState::GameOver), systems::despawn_menu);
--- src/menus/systems.rs
+++ src/menus/systems.rs
@@ -1,124 +1,32 @@
+use belly::prelude::*;
 use bevy::prelude::*;
 
-use crate::{
-    states::MainState,
-    ui::{spawn_button, BorderTexture, TextBox, UiFont},
-};
+use crate::states::MainState;
 
-use super::Menu;
+use super::StartGameEvent;
 
-pub fn main_menu(mut commands: Commands, border_texture: Res<BorderTexture>, font: Res<UiFont>) {
-    let start_button = spawn_button(&mut commands, &border_texture, &font, "Start", (150., 75.));
-    let text = TextBundle {
-        style: Style {
-            width: Val::Auto,
-            height: Val::Auto,
-            ..default()
-        },
-        text: Text {
-            sections: vec![TextSection {
-                value: "Roguelike".to_string(),
-                style: TextStyle {
-                    font: font.0.clone(),
-                    font_size: 90.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            }],
-            ..default()
-        },
-        ..default()
-    };
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                column_gap: Val::Px(30.0),
-                row_gap: Val::Px(30.0),
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                flex_direction: FlexDirection::Column,
-                ..default()
-            },
-            ..default()
-        })
-        .insert(Menu)
-        .with_children(|parent| {
-            parent.spawn(text);
-        })
-        .push_children(&[start_button]);
+pub fn main_menu(mut commands: Commands) {
+    commands.add(eml! {
+        <body id="menu" s:width="100%" s:height="100%">
+            <div s:font="bold" s:font-size="90px">"Roguelike"</div>
+            <button on:press=|ctx|{ctx.send_event(StartGameEvent)}><div s:font="bold" s:padding="5px 10px 5px 10px" s:font-size="45px">"START"</div></button>
+        </body>
+    });
 }
 
-pub fn game_over_menu(
-    mut commands: Commands,
-    border_texture: Res<BorderTexture>,
-    font: Res<UiFont>,
-) {
-    let text = TextBundle {
-        style: Style {
-            width: Val::Auto,
-            height: Val::Auto,
-            ..default()
-        },
-        text: Text {
-            sections: vec![TextSection {
-                value: "Game Over!".to_string(),
-                style: TextStyle {
-                    font: font.0.clone(),
-                    font_size: 90.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            }],
-            ..default()
-        },
-        ..default()
-    };
-    let restart_button = spawn_button(
-        &mut commands,
-        &border_texture,
-        &font,
-        "Restart",
-        (150., 75.),
-    );
-
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                column_gap: Val::Px(30.0),
-                row_gap: Val::Px(30.0),
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                flex_direction: FlexDirection::Column,
-                ..default()
-            },
-            ..default()
-        })
-        .insert(Menu)
-        .with_children(|parent| {
-            parent.spawn(text);
-        })
-        .push_children(&[restart_button]);
+pub fn game_over_menu(mut commands: Commands) {
+    commands.add(eml! {
+        <body id="menu" s:width="100%" s:height="100%">
+            <div s:font="bold" s:font-size="90px">"Game Over!"</div>
+            <button on:press=|ctx|{ctx.send_event(StartGameEvent)}><div s:font="bold" s:font-size="45px">"RESTART"</div></button>
+        </body>
+    });
 }
 
-pub fn despawn_menu(mut commands: Commands, query: Query<Entity, With<Menu>>) {
-    for entity in query.iter() {
-        commands.entity(entity).despawn_recursive();
-    }
+pub fn despawn_menu(mut elements: Elements) {
+    elements.select("#menu").remove();
 }
 
-pub fn menu_button_system(
-    mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<TextBox>)>,
-    mut state: ResMut<NextState<MainState>>,
-) {
-    for interaction in &mut interaction_query {
-        match *interaction {
-            Interaction::Pressed => {
-                state.set(MainState::Game);
-            }
-            Interaction::Hovered => {}
-            Interaction::None => {}
-        }
-    }
+pub fn start_game_event_system(mut state: ResMut<NextState<MainState>>) {
+    state.set(MainState::Game);
 }
